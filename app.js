84a9244c7c5341436daaa787d92a6a4e40760d93
Meet710 <meetvaghani611@gmail.com>
2024-05-30 00:36:57 +0530

    swagger

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const database = require('./config/database')
 const author = require('./routes/author')
 const book = require('./routes/book')
 const APIError = require('./utils/APIError')
+const swaggerSetup = require('./routes/swagger');
+
 
 
 database()
 const app = express()
 const port = process.env.PORT
+app.use(cors());
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : false}))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.use('/author', author)
-app.use('/book' , book)
+app.use('/book', book)
+app.use('/', swaggerSetup)
 app.use(function (err, req, res, next) {
     // Check if the error is an instance of APIError
     if (err instanceof APIError) {
